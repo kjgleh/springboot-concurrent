bbb59b3de6fad64bab9f08d6a80e6ad7328da5b7
김정근 <gogoogjg@tripstore.kr>
2023-03-25 22:42:03 +0900

    initialize the project

---
+++ src/main/kotlin/me/kjgleh/springbootconcurrent/SpringbootConcurrentApplication.kt
@@ -0,0 +1,11 @@
+package me.kjgleh.springbootconcurrent
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootConcurrentApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootConcurrentApplication>(*args)
+}
